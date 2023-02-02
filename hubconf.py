fd281bcca9b06e239ea44960ac9b8fcc02e9c203
roy <gkstjd1201@naver.com>
2023-02-02 11:21:22 +0000

    test

--- hubconf.py
+++ hubconf.py
@@ -1,5 +1,5 @@
 
-model_name = "DTS_for_dialouge"
+model_name = "Domain_Topic_Segmentation"
 model_description = "dialouge Domain Topic Segmentation model"
 model_author = "Hanseong Kim"
 model_url = "https://github.com/dataKim1201/dialouge_Topic_Segmentation"
@@ -18,7 +18,7 @@ def Domain_Topic_Segmentation(pretrained_id = 'klue/roberta-large'):
     # code_url = "https://github.com/boostcampaitech4lv23nlp1/final-project-level3-nlp-07/blob/master/DTS/model.py"
 
     model = CSModel(pretrained_id=pretrained_id)
-    model.load_state_dict(torch.load('Domain_Topic_segmentor.pt'))
+    model.load_state_dict(torch.load('./Domain_Topic_segmentor.pt'))
     return model
 
 # python -m torch.hub publish-model Domain_Topic_Segmentation ./hubconf.py --trust_repo=True
