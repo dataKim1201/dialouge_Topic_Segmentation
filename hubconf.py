3b16cd733e467d05243368bf1475ff411140a9f8
roy <gkstjd1201@naver.com>
2023-02-02 10:24:53 +0000

    ets

--- hubconf.py
+++ hubconf.py
@@ -18,5 +18,5 @@ def DTS_for_dialouge(pretrained_id = 'klue/roberta-large'):
     # code_url = "https://github.com/boostcampaitech4lv23nlp1/final-project-level3-nlp-07/blob/master/DTS/model.py"
 
     model = CSModel(pretrained_id=pretrained_id)
-    model = model.load_state_dict(torch.load('/opt/ml/input/dialouge_Topic_Segmentation/Domain_Topic_segmentor.pt'))
+    model = model.load_state_dict(torch.load('/opt/ml/Domain_Topic_segmentor.pt'))
     return model
